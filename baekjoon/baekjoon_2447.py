06767a99b38aa287788190c44765834a4ce2f6b4
swwho96 <swwho96@gmail.com>
2022-06-19 17:36:20 +0900

    재귀호출로 별 찍기

---
+++ baekjoon/baekjoon_2447.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+def star(n):
+	if n == 1: return ['*'] 
+		
+	stars = star(n//3) 
+	L = [] # 별을 출력할 리스트
+	
+	for s in stars: L.append(s*3) # 상 구간
+	for s in stars: L.append(s+' '*(n//3)+s) # 중 구간
+	for s in stars: L.append(s*3) # 하 구간
+	print(L)
+	return L
+
+print('\n'.join(star(num)))
