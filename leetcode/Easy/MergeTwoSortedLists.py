366d4430b8a84acc7e83446adb40068a3cd555fc
swwho96 <swwho96@gmail.com>
2022-07-06 01:13:55 +0900

    21. Merge Two Sorted Lists (미완성)

---
+++ leetcode/Easy/MergeTwoSortedLists.py
@@ -0,0 +1,9 @@
+'''
+정렬된 두 리스트를 하나의 정렬된 리스트로 결합하는 문제
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
