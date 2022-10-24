5e338391f516bf5e2e937796f9917178f198de8c
swwho96 <swwho96@gmail.com>
2022-10-24 22:15:59 +0900

    렌선 자르기

--- baekjoon/container2/baekjoon_1654.py
+++ baekjoon/container2/baekjoon_1654.py
@@ -4,17 +4,14 @@ len_lines = []
 for _ in range(N):
     len_lines.append(int(sys.stdin.readline().rstrip()))
 left = max(len_lines)
-right = 0
-while left - right > 1:
+right = 1
+while left >= right:
     can_make_len = 0
     pivot = int((left+right) / 2)
     for l in len_lines:
         can_make_len += int(l / pivot)
     if can_make_len >= K:
-        right = pivot
+        right = pivot + 1
     else:
-        left = pivot
-can_make_len = 0
-for l in len_lines:
-    can_make_len += int(l/left)
-print(left if can_make_len >= K else right)
+        left = pivot - 1
+print(left)
