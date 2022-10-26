c220f1ff9dbe2a7e0097881f1c0921dbf8f1e11e
swwho96 <swwho96@gmail.com>
2022-10-27 01:32:19 +0900

    스택 수열

---
+++ baekjoon/container2/baekjoon_1874.py
@@ -0,0 +1,41 @@
+import sys
+n = int(sys.stdin.readline().rstrip())
+num = []
+result = []
+push_and_pop = []
+for _ in range(n):
+    num.append(int(sys.stdin.readline().rstrip()))
+
+num = num[::-1]
+start = num[-1]
+max = start
+for i in range(1, start+1):
+    result.append(i)
+    push_and_pop.append('+')
+
+while True:
+    if len(result) <= 0:
+        if len(num) <= 0:
+            break
+        else:
+            for i in range(max+1, num[-1]+1):
+                result.append(i)
+                push_and_pop.append('+')
+            max = num[-1]
+    if len(num) <= 0:
+        push_and_pop = ['NO']
+        break
+    if num[-1] >= result[-1]:
+        for i in range(max+1, num[-1]+1):
+            result.append(i)
+            push_and_pop.append('+')
+        if max < num[-1]: max = num[-1]
+    else:
+        push_and_pop = ['NO']
+        break
+    num.pop()
+    result.pop()
+    push_and_pop.append('-')
+
+for i in push_and_pop:
+    print(i)
