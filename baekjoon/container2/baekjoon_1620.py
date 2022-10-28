0187969ea51cbbbe00f403a5fb13e57a876ef40c
swwho96 <swwho96@gmail.com>
2022-10-28 13:22:51 +0900

    나는야 포켓몬 마스터 이다솜

---
+++ baekjoon/container2/baekjoon_1620.py
@@ -0,0 +1,12 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+pokatmon_dict = {}
+# 포켓몬 도감 생성
+for i in range(1,N+1):
+    name = sys.stdin.readline().rstrip()
+    pokatmon_dict[name] = i
+    pokatmon_dict[str(i)] = name
+# 문제
+for i in range(M):
+    question = sys.stdin.readline().rstrip()
+    print(pokatmon_dict[question])
