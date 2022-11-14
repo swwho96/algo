156fa585ad131c545144684d0278cdcd6eb397f0
swwho96 <swwho96@gmail.com>
2022-11-14 23:55:48 +0900

    Minimum Window Substring

---
+++ leetcode/Hard/MinimumWindowSubstring.py
@@ -0,0 +1,20 @@
+from collections import Counter
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = Counter(t)
+        miss = len(t)
+        left = start = end = 0
+        for right, char in enumerate(s, 1):
+            miss -= need[char] > 0
+            need[char] -= 1
+            if miss == 0:
+                while left < right and need[s[left]] != 0:
+                    need[s[left]] += 1
+                    left += 1
+                
+                if not end or (right-left) <= (end-start):
+                    start, end = left, right
+                    need[s[left]] += 1
+                    miss += 1
+                    left += 1
+        return s[start:end]
