8d964f8e061d88b28ccfffcc0e9de728494d004a
swwho96 <swwho96@gmail.com>
2022-11-27 23:19:50 +0900

    징검다리 건너기

---
+++ programmers/Level_3/CrossStones.py
@@ -0,0 +1,20 @@
+def check(stones, mid, k):
+    cnt = 0
+    for s in stones:
+        if s < mid:
+            cnt += 1
+        else:
+            cnt = 0
+        if cnt == k:
+            return False
+    return True
+
+def solution(stones, k):
+    left, right = 0, max(stones)
+    while left < right - 1:
+        mid = (left+right) // 2
+        if check(stones, mid, k):
+            left = mid
+        else:
+            right = mid
+    return left
