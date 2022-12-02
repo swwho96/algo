e58e010293aa85bc134eac894c592718a1027226
swwho96 <swwho96@gmail.com>
2022-12-02 23:50:46 +0900

    46. Permutations

--- leetcode/Medium/Permutations.py
+++ leetcode/Medium/Permutations.py
@@ -1,25 +1,18 @@
-# List = list
-# from itertools import combinations, permutations
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         return [[*permut] for permut in list(permutations(nums, len(nums)))]
+# 조합 활용
+from itertools import combinations, permutations
+def permute(self, nums: List[int]) -> List[List[int]]:
+    return [[*permut] for permut in list(permutations(nums, len(nums)))]
 
-# s = Solution()
-# print(s.permute([1,2,3]))
-
-class Solution:
-    def permute(self, nums):
-        res = []
-        n = len(nums)
-        stack = [[0, nums, []]]
-        while stack:
-            length, integers, ans = stack.pop()
-            if length == n:
-                res.append(ans)
-            else:
-                for i in range(len(integers)):
-                    stack.append([length+1, integers[:i]+integers[i+1:], ans+[integers[i]]])
-        return res
-
-s = Solution()
-print(s.permute([1,2,3]))
+# DFS 활용
+def permute(self, nums):
+    res = []
+    n = len(nums)
+    stack = [[0, nums, []]]
+    while stack:
+        length, integers, ans = stack.pop()
+        if length == n:
+            res.append(ans)
+        else:
+            for i in range(len(integers)):
+                stack.append([length+1, integers[:i]+integers[i+1:], ans+[integers[i]]])
+    return res
