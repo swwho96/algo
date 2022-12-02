d9b6e775b45454ed724015c2af911d83f0d58c95
swwho96 <swwho96@gmail.com>
2022-12-02 23:52:20 +0900

    77. Combinations

--- leetcode/Medium/Combinations.py
+++ leetcode/Medium/Combinations.py
@@ -1,6 +1,19 @@
+# 조합 활용
 from itertools import combinations
-List = list
-class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        n_list = [i for i in range(1,n+1)]
-        return list(combinations(n_list, k))
+def combine(self, n: int, k: int) -> List[List[int]]:
+    n_list = [i for i in range(1,n+1)]
+    return list(combinations(n_list, k))
+
+# DFS 활용
+def combine(n, k):
+    nums = [i for i in range(1, n+1)]
+    result = []
+    stack = [[0, nums, []]]
+    while stack:
+        length, integers, ans = stack.pop()
+        if length == k:
+            result.append(ans)
+        else:
+            for i in range(len(integers)):
+                stack.append([length+1, integers[i+1:], ans+[integers[i]]])
+    return result
