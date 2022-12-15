7e5b2a33b6c63e207ff25c8eab4c30d7fa5a4dd9
swwho96 <swwho96@gmail.com>
2022-12-15 15:48:40 +0900

    test file

--- algorithm_organize/test.py
+++ algorithm_organize/test.py
@@ -27,30 +27,74 @@
 
 # print(icecream)
 
-'''-------------------------------------------------'''
+#'''-------------------------------------------------'''
 
 # 단어 분리 하기
-from queue import deque
-word = 'centerminus'
-word_dict = ['cent', 'center', 'term', 'terminus', 'min', 'us', 'rm', 'minus']
-q = [word[0]]
-result = []
-new = []
-def dfs(new, word):
-    while q:
-        char = q.pop()
-        for w in word_dict:
-            if len(word) < 2:
-                result.append(new)
-                print(new)
-                new = []
-                break
-            if word.startswith(w):
-                new.append(w)
-                tmp = w[-1] + word.replace(w, '')
-                q.append(tmp[0])
-                dfs(new, tmp)
-            new.pop()
+# from queue import deque
+# word = 'centerminus'
+# word_dict = ['cent', 'center', 'term', 'terminus', 'min', 'us', 'rm', 'minus']
+# q = [word[0]]
+# result = []
+# new = []
+# def dfs(new, word):
+#     while q:
+#         char = q.pop()
+#         for w in word_dict:
+#             if len(word) < 2:
+#                 result.append(new)
+#                 print(new)
+#                 new = []
+#                 break
+#             if word.startswith(w):
+#                 new.append(w)
+#                 tmp = w[-1] + word.replace(w, '')
+#                 q.append(tmp[0])
+#                 dfs(new, tmp)
+#             new.pop()
 
-dfs(new, word)
-print(result)
+# dfs(new, word)
+# print(result)
+
+direction_x = [-1,0,1,0]
+direction_y = [0,1,0,-1]
+
+def TurnLeft(di):
+    return di - 1 if di >= 1 else 3
+
+def Move(x, y, di):
+    return x+direction_x[di], y+direction_y[di]
+
+def Back(x, y, di):
+    return x-direction_x[di], y-direction_y[di]
+
+N, M = map(int, input().split())
+x, y, di = map(int, input().split())
+maps = []
+for i in range(N):
+    maps.append(list(map(int, input().split())))
+cnt = 1
+
+while True:
+    print(maps)
+    flag = False
+    for _ in range(4):
+        di = TurnLeft(di)
+        nx, ny = Move(x, y, di)
+        if 0<=nx<N and 0<=ny<M and maps[nx][ny] == 0:
+            flag = True
+            cnt += 1
+            maps[x][y] = 9
+            maps[nx][ny] = 9
+            x, y = nx, ny
+            break
+    if flag is True:
+        continue
+    else:
+        nx, ny = Back(x, y, di)
+        if maps[nx][ny] == 1:
+            print('The End')
+            break
+        else:
+            x, y = nx, ny
+
+print(cnt)
