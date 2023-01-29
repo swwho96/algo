578e592c34b22e385e17d6b803efa4f894e0a0ae
swwho96 <swwho96@gmail.com>
2023-01-29 14:56:17 +0900

    이중 우선순위 큐

--- baekjoon/container3/baekjoon_7662.py
+++ baekjoon/container3/baekjoon_7662.py
@@ -1,25 +1,37 @@
 import sys
 input = sys.stdin.readline
 import heapq
-from queue import PriorityQueue
+from collections import defaultdict
 T = int(input())
 for _ in range(T):
     n = int(input())
-    q = PriorityQueue()
+    max_q, min_q = [], []
+    number_cnt_dict = defaultdict(int)
     for _ in range(n):
         c, num = input().split()
         if c == 'I':
-            heapq.heappush(q, int(num))
+            number_cnt_dict[int(num)] += 1
+            heapq.heappush(min_q, int(num))
+            heapq.heappush(max_q, -int(num))
         elif c == 'D':
-            if not q:
-                continue
-            else:
-                if num == '1':
-                    q.pop(q.index(max(q)))
-                elif num == '-1':
-                    heapq.heappop(q)
-    # 비어있다면 'EMPTY', 아니라면 [최대, 최소]
-    if q:
-        print(max(q), min(q))
+            if num == '1':
+                while max_q:
+                    tmp = -heapq.heappop(max_q)
+                    if number_cnt_dict[tmp] > 0:
+                        number_cnt_dict[tmp] -= 1
+                        break
+            elif num == '-1':
+                while min_q:
+                    tmp = heapq.heappop(min_q)
+                    if number_cnt_dict[tmp] > 0:
+                        number_cnt_dict[tmp] -= 1
+                        break
+    result = sorted(number_cnt_dict.items(), key=lambda x: x[0], reverse=True)
+    answer = []
+    for i, v in result:
+        if v > 0:
+            answer.append(i)
+    if answer:
+        print(answer[0], answer[-1])
     else:
         print('EMPTY')
