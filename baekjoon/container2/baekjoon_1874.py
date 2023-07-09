a964637ad9a0349b88e61ed4d79ed51989d587cc
swwho96 <swwho96@gmail.com>
2023-07-09 19:08:47 +0900

    스택 수열

--- baekjoon/container2/baekjoon_1874.py
+++ baekjoon/container2/baekjoon_1874.py
@@ -1,41 +1,71 @@
+# import sys
+# n = int(sys.stdin.readline().rstrip())
+# num = []
+# result = []
+# push_and_pop = []
+# for _ in range(n):
+#     num.append(int(sys.stdin.readline().rstrip()))
+
+# num = num[::-1]
+# start = num[-1]
+# max = start
+# for i in range(1, start+1):
+#     result.append(i)
+#     push_and_pop.append('+')
+
+# while True:
+#     if len(result) <= 0:
+#         if len(num) <= 0:
+#             break
+#         else:
+#             for i in range(max+1, num[-1]+1):
+#                 result.append(i)
+#                 push_and_pop.append('+')
+#             max = num[-1]
+#     if len(num) <= 0:
+#         push_and_pop = ['NO']
+#         break
+#     if num[-1] >= result[-1]:
+#         for i in range(max+1, num[-1]+1):
+#             result.append(i)
+#             push_and_pop.append('+')
+#         if max < num[-1]: max = num[-1]
+#     else:
+#         push_and_pop = ['NO']
+#         break
+#     num.pop()
+#     result.pop()
+#     push_and_pop.append('-')
+
+# for i in push_and_pop:
+#     print(i)
+
 import sys
-n = int(sys.stdin.readline().rstrip())
-num = []
+input = sys.stdin.readline
+n = int(input())
 result = []
-push_and_pop = []
+tmp_stack = []
+stack = []
+k, idx = 1, 0
 for _ in range(n):
-    num.append(int(sys.stdin.readline().rstrip()))
+    stack.append(int(input()))
 
-num = num[::-1]
-start = num[-1]
-max = start
-for i in range(1, start+1):
-    result.append(i)
-    push_and_pop.append('+')
-
-while True:
-    if len(result) <= 0:
-        if len(num) <= 0:
-            break
-        else:
-            for i in range(max+1, num[-1]+1):
-                result.append(i)
-                push_and_pop.append('+')
-            max = num[-1]
-    if len(num) <= 0:
-        push_and_pop = ['NO']
-        break
-    if num[-1] >= result[-1]:
-        for i in range(max+1, num[-1]+1):
-            result.append(i)
-            push_and_pop.append('+')
-        if max < num[-1]: max = num[-1]
+while idx <= n-1:
+    now = stack[idx]
+    if not tmp_stack or now > tmp_stack[-1]:
+        result.append('+')
+        tmp_stack.append(k)
+        k += 1
+    elif now == tmp_stack[-1]:
+        result.append('-')
+        tmp_stack.pop()
+        idx += 1
     else:
-        push_and_pop = ['NO']
+        result = 'NO'
         break
-    num.pop()
-    result.pop()
-    push_and_pop.append('-')
 
-for i in push_and_pop:
-    print(i)
+if result != 'NO':
+    for r in result:
+        print(r)
+else:
+    print(result)
