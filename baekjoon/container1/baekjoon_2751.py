b44eca6038bb6a8c5da431330cd40e5510349ac3
swwho96 <swwho96@gmail.com>
2023-07-18 12:15:44 +0900

    수 정렬하기 2

--- baekjoon/container1/baekjoon_2751.py
+++ baekjoon/container1/baekjoon_2751.py
@@ -10,3 +10,20 @@ numbers.sort()
 
 for num in numbers:
     print(num)
+
+# -----------------------------------
+
+import sys
+input = sys.stdin.readline
+n = int(input())
+array = []
+for _ in range(n):
+    array.append(int(input()))
+
+def merge_sort(s, e):
+    if e-s < 1:
+        return
+    mid = int(s+(e-s) / 2)
+    merge_sort(s, mid)
+    merge_sort(mid, e)
+    
