a54d67f7ae83b952079529a41ff20548c1eee8ea
swwho96 <swwho96@gmail.com>
2023-09-22 20:52:13 +0900

    level. 2 삼각 달팽이

--- review.py
+++ review.py
@@ -1,29 +1,61 @@
-import sys
-input = sys.stdin.readline
-n = int(input())
-m = int(input())
-INF = int(1e9)
-bus_info = [[INF] * (n+1) for _ in range(n+1)]
-for _ in range(m):
-    a, b, c = map(int, input().split())
-    if bus_info[a][b] > c: 
-        bus_info[a][b] = c
+def solution(n):
+    answer = []
+    if n == 1 : return [1]
+    if n == 2 : return [1,2,3]
+    # 탑 쌓기
+    board = [[1] * i for i in range(1, n+1)]
+    # 탑에 숫자 넣기
+    flag = True
+    row, col = 0, 0
+    while flag:
+        # 왼쪽 - 아래방향
+        while True:
+            nrow, ncol = row + 1, col
+            if nrow < n and board[nrow][ncol] == 1:
+                board[nrow][ncol] = board[row][col] + 1
+                row, col = nrow, col
+            else:
+                break        
+        # 아래 - 오른쪽 방향
+        while True:
+            nrow, ncol = row, col + 1 
+            if ncol <= nrow and board[nrow][ncol] == 1:
+                board[nrow][ncol] = board[row][col] + 1
+                row, col = nrow, ncol
+            else:
+                break
+        # 오른쪽 - 왼쪽 위 방향
+        while True:
+            nrow, ncol = row - 1, col - 1
+            if 0 < nrow and 0 < ncol and board[nrow][ncol] == 1:
+                board[nrow][ncol] = board[row][col] + 1
+                row, col = nrow, ncol
+            else:
+                if board[row+1][col] != 1:
+                    flag = False
+                break
+    # 탑의 각 행 모으기
+    for b in board:
+        answer.extend(b)
+    return answer
 
-# 자기 자신으로 가는 비용은 0
-for i in range(1, n+1):
-    bus_info[i][i] = 0
+# ------------------------------------------------------------------------------------------
 
-# 버스 정보 업데이트
-for k in range(1, n+1):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            bus_info[i][j] = min(bus_info[i][j], bus_info[i][k]+bus_info[k][j])
+def solution(n):
+    res = [[0] * i for i in range(1, n+1)]
+    row, col = -1, 0
+    num = 1
 
-# 정보 출력
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if bus_info[i][j] == INF:
-            print(0, end=' ')
-        else:
-            print(bus_info[i][j], end=' ')
-    print()
+    for i in range(n):
+        for _ in range(i, n):
+            angle = i % 3
+            if angle == 0:
+                row += 1
+            elif angle == 1:
+                col += 1
+            elif angle == 2:
+                row -= 1
+                col -= 1
+            res[row][col] = num
+            num += 1
+    return [j for i in res for j in i]
