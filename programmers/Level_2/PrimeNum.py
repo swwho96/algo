2349b0d31271c03b1d501e145f9dbbfcf22f938b
swwho96 <swwho96@gmail.com>
2023-10-10 14:50:24 +0900

    Level 2. 소수 찾기 풀이 추가

--- programmers/Level_2/PrimeNum.py
+++ programmers/Level_2/PrimeNum.py
@@ -19,3 +19,23 @@ def solution(numbers):
             answer += 1
             
     return answer
+
+# ------------------------------------------------------------------------------------------
+
+from itertools import permutations
+def solution(numbers):
+    answer = 0
+    paper_num = set()
+    # 가능한 경우의 수 구하기
+    for i in range(1, len(numbers)+1):
+        paper_num |= set(map(int, map(''.join, permutations(list(numbers), i))))
+    paper_num -= set(range(0,2))
+    # 소수 판별하기
+    for N in paper_num:
+        flag = True
+        for i in range(2, int(N**0.5)+1):
+            if N % i == 0:
+                flag = False
+                break
+        if flag: answer += 1
+    return answer
