76a9368f4d0fcb49f0c65502fc4952fbdb95d5d7
swwho96 <swwho96@gmail.com>
2023-11-09 23:49:51 +0900

    사다리조작 풀이 변경

--- baekjoon/container5/baekjoon_15684.py
+++ baekjoon/container5/baekjoon_15684.py
@@ -1,49 +1,44 @@
 import sys
-import copy
-from collections import deque
 input = sys.stdin.readline
-n, m, h = map(int, input().split())
-graph = [[0]*(h+1) for _ in range(n+1)]
-for _ in range(m):
+
+N, M, H = map(int, input().split())
+board = [[0] * (N+1) for _ in range(H+1)]
+for _ in range(M):
     a, b = map(int, input().split())
-    graph[b][a] = 1
+    board[a][b] = 1
 
-need_bridge = int(1e9) # 결과
-
-# 사다리 놓을 수 있는 경우 찾기
-q = []
-for i in range(1,n):
-    for j in range(1,h+1):
-        if graph[i][j] == 0 and graph[i+1][j] == 0 and graph[i-1][j] == 0:
-            q.append((i, j))
-
-# i번이 i번에 도착하는지 확인
-def check(g):
-    for i in range(1,n+1):
-        idx = i
-        for j in range(1, h+1):
-            if g[idx][j] == 1:
-                idx += 1
-            elif g[idx-1][j] == 1:
-                idx -= 1
-        if idx !=  i:
+def check():
+    for i in range(1, N+1):
+        now = i
+        for b in board:
+            if b[now-1] == 1:
+                now -= 1
+            elif b[now] == 1:
+                now += 1
+        if i != now:
             return False
     return True
 
-
-# 사다리를 하나씩 추가해가며 되는 경우 확인
-def bfs(q, cnt):
-    global need_bridge
-    if not q or cnt > need_bridge or cnt > 3:
+def dfs(depth, i):
+    global result
+    if depth >= result:
         return
-    if check(graph):
-        need_bridge = min(need_bridge, cnt)
+    if check():
+        result = depth
         return
-    for i, (col, row) in enumerate(q):
-        if graph[col-1][row] == 0 and graph[col+1][row] == 0:
-            graph[col][row] = 1 
-            bfs(q[i+1:], cnt+1)
-            graph[col][row] = 0
+    for c in range(i, len(combi)):
+        x, y = combi[c]
+        if board[x][y-1] == 0 and board[x][y+1] == 0:
+            board[x][y] = 1
+            dfs(depth+1, c+1)
+            board[x][y] = 0
 
-bfs(q, 0)
-print(-1 if need_bridge > 3 else need_bridge)
+combi = []
+for i in range(1, H+1):
+    for j in range(1, N):
+        if board[i][j] == 0 and board[i][j-1] == 0 and board[i][j+1] == 0:
+            combi.append((i,j))
+
+result = 4
+dfs(0, 0)
+print(result if result < 4 else -1)
