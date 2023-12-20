5af43cf5fb82ba99eadbc46764a3a3a828198878
swwho96 <swwho96@gmail.com>
2023-12-20 23:58:05 +0900

    백준 1920 리뷰

--- review.py
+++ review.py
@@ -1,51 +1,34 @@
+# import sys
+# input = sys.stdin.readline
+
+# N = int(input())
+# A = set(map(int,input().split()))
+# M = int(input())
+# numbers = list(map(int, input().split()))
+# for n in numbers:
+#     if n in A:
+#         print(1)
+#     else:
+#         print(0)
+
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-N, M, V = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-
-for g in graph:
-    g.sort()
-
-def bfs(start):
-    q = deque([start])
-    visited[start] = True
-    while q:
-        now = q.popleft()
-        print(now, end=' ')
-        for i in graph[now]:
-            if visited[i] is False:
-                visited[i] = True
-                q.append(i)
-
-# def dfs(start):
-#     stack = [start]
-#     visited[start] = True
-#     while stack:
-#         now = stack.pop()
-#         print(now, end=' ')
-#         for i in graph[now]:
-#             if visited[i] is False:
-#                 visited[i] = True
-#                 dfs(i)
-def dfs(start):
-    stack = [start]
-    visited[start] = True
-    while stack:
-        now = stack.pop()
-        print(now, end=' ')
-        for i in sorted(graph[now], reverse=True):
-            if visited[i] is False:
-                visited[i] = True
-                stack.append(i)
-
-visited = [False] * (N+1)
-dfs(V)
-print()
-visited = [False] * (N+1)
-bfs(V)
+N = int(input())
+A = list(map(int,input().split()))
+A.sort()
+M = int(input())
+numbers = list(map(int, input().split()))
+for n in numbers:
+    left, right = 0, len(A)-1
+    flag = False
+    while left <= right:
+        mid = (left+right) // 2
+        if A[mid] == n:
+            flag = True
+            break
+        elif mid > n:
+            right = mid-1
+        else:
+            left = mid+1
+    print(1) if flag is True else print(0)
