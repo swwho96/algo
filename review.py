b244cc278e78fd3bce62ee25915d69fdcf614210
swwho96 <swwho96@gmail.com>
2023-12-23 14:53:35 +0900

    백준 줄세우기 리뷰

--- review.py
+++ review.py
@@ -2,34 +2,27 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-N = int(input())
-
-# 그래프
-indgree = [0] * (N+1)
-costs = [0] * (N+1)
+N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
-for i in range(1,N+1):
-    tmp = list(map(int, input().split()))
-    tmp.pop()
-    costs[i] = tmp[0]
-    for building in tmp[1:]:
-        graph[building].append(i)
-        indgree[i] += 1
+indgree = [0] * (N+1)
 
-# 진입차수 0인 건물 짓기
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indgree[b] += 1
+
 q = deque()
 for i in range(1, N+1):
     if indgree[i] == 0:
         q.append(i)
-
-result = [0] * (N+1)
+    
+result = []
 while q:
     now = q.popleft()
-    for b in graph[now]:
-        indgree[b] -= 1
-        result[b] = max(result[b], result[now]+costs[now])
-        if indgree[b] == 0:
-            q.append(b)
+    result.append(str(now))
+    for i in graph[now]:
+        indgree[i] -= 1
+        if indgree[i] == 0:
+            q.append(i)
 
-for i in range(1, N+1):
-    print(result[i]+costs[i])
+print(' '.join(result))
