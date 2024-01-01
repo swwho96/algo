90305d05131a4c06c048218cafc0f610af6d392d
swwho96 <swwho96@gmail.com>
2024-01-01 15:28:51 +0900

    백준 구간합구하기5 리뷰

--- review.py
+++ review.py
@@ -1,26 +1,24 @@
 import sys
 input = sys.stdin.readline
+N, M = map(int, input().split())
 
-N = int(input())
-M = int(input())
-INF = int(10e9)
-graph = [[INF]*(N+1) for _ in range(N+1)]
+# 배열 담기
+board = []
+for i in range(N):
+    board.append(list(map(int, input().split())))
+
+# 질의 담기
+queries = []
 for _ in range(M):
-    a, b, c = map(int, input().split())
-    graph[a][b] = min(graph[a][b], c)
+    queries.append(list(map(int, input().split())))
 
-for i in range(1,N+1):
-    graph[i][i] = 0
-
-for k in range(1, N+1):
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
-
+# 구간합 구하기
+S = [[0]*(N+1) for _ in range(N+1)]
 for i in range(1, N+1):
     for j in range(1, N+1):
-        if graph[i][j] == INF:
-            print(0, end=' ')
-        else:
-            print(graph[i][j], end=' ')
-    print()
+        S[i][j] = S[i-1][j] + S[i][j-1] - S[i-1][j-1] + board[i-1][j-1]
+
+for q in queries:
+    x1, y1, x2, y2 = q
+    answer = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1]
+    print(answer)
