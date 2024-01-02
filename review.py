644ea2879e7ce310a30d6c82bd6cfbbdc582b771
swwho96 <swwho96@gmail.com>
2024-01-02 14:55:54 +0900

    백준 주몽 리뷰

--- review.py
+++ review.py
@@ -1,17 +1,55 @@
+'''
+두 재료를 합쳐 M이 되는 경우를 찾는 문제
+합이 M이 되는 조합을 찾고, 각 재료의 개수에 따른 경우의 수를 구하여 결과에 더한다
+'''
+
 import sys
 input = sys.stdin.readline
 N = int(input())
-left, right = 1, 1
-answer = 1
-cnt = 0
-while left <= right <= N:
-    if answer < N:
-        right += 1
-        answer += right
-    elif answer >= N:
-        if answer == N:
-            cnt += 1
-        answer -= left
+M = int(input())
+materials = list(map(int, input().split()))
+
+cnt = {}
+for m in materials:
+    cnt[m] = cnt[m] + 1 if m in cnt else 1
+
+result = 0
+while cnt:
+    a = list(cnt.keys())[0]
+    b = M - a
+    if a == b:
+        if cnt[a] == 1:
+            del cnt[a]
+        else:
+            result += ((cnt[a] * cnt[a]) // 2)
+            del cnt[a]
+    elif b in cnt:
+        result += (cnt[a] * cnt[b])
+        del cnt[a]
+        del cnt[b]
+    else:
+        del cnt[a]
+
+print(result)
+
+# -----------------------------------------------------
+import sys
+input = sys.stdin.readline
+N = int(input())
+M = int(input())
+materials = list(map(int, input().split()))
+materials.sort()
+left, right = 0, N-1
+
+result = 0
+while left < right:
+    tmp = materials[left] + materials[right]
+    if tmp == M:
+        result += 1
+        left, right = left+1, right-1
+    elif tmp > M:
+        right -= 1
+    else:
         left += 1
 
-print(cnt)
+print(result)
