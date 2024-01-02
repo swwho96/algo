b12fa6c7d57c16968a69ae59792a0fe502438d77
swwho96 <swwho96@gmail.com>
2024-01-02 14:29:11 +0900

    백준 수들의합5 리뷰

--- review.py
+++ review.py
@@ -1,24 +1,17 @@
 import sys
 input = sys.stdin.readline
-N, M = map(int, input().split())
+N = int(input())
+left, right = 1, 1
+answer = 1
+cnt = 0
+while left <= right <= N:
+    if answer < N:
+        right += 1
+        answer += right
+    elif answer >= N:
+        if answer == N:
+            cnt += 1
+        answer -= left
+        left += 1
 
-# 배열 담기
-board = []
-for i in range(N):
-    board.append(list(map(int, input().split())))
-
-# 질의 담기
-queries = []
-for _ in range(M):
-    queries.append(list(map(int, input().split())))
-
-# 구간합 구하기
-S = [[0]*(N+1) for _ in range(N+1)]
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        S[i][j] = S[i-1][j] + S[i][j-1] - S[i-1][j-1] + board[i-1][j-1]
-
-for q in queries:
-    x1, y1, x2, y2 = q
-    answer = S[x2][y2] - S[x1-1][y2] - S[x2][y1-1] + S[x1-1][y1-1]
-    print(answer)
+print(cnt)
