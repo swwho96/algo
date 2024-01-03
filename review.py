46fd6094f998c5cbce85d3f217a7025c547c68e6
swwho96 <swwho96@gmail.com>
2024-01-03 14:54:36 +0900

    백준 오큰수 리뷰

--- review.py
+++ review.py
@@ -1,31 +1,19 @@
-''''
-1부터 N까지 숫자를 하나씩 스택에 담거나 빼면서, 주어진 수열을 만든다
+'''
+수열의 각 원소에 대해, 가장 가까운 자신보다 큰 수를 찾아 출력한다
+
 '''
 
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
-A = deque([])
-for _ in range(N):
-    A.append(int(input()))
+A = list(map(int, input().split()))
+q = []
+answer = [-1] * N
+for i, num in enumerate(A):
+    while q and q[-1][1] < num:
+        a, b = q.pop()
+        answer[a] = num
+    q.append([i, num])
 
-q = deque([])
-answer = []
-i = 1
-while A:
-    if q and A[0] == q[-1]:
-        answer.append('-')
-        q.pop()
-        A.popleft()
-    elif q and q[-1] > A[0]:
-        answer = ['NO']
-        break
-    else:
-        q.append(i)
-        i += 1
-        answer.append('+')
-
-for a in answer:
-    print(a)
+print(' '.join(map(str, answer)))
