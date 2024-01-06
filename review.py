c65a90162bfa1a816bbc641ecf840538935957c6
swwho96 <swwho96@gmail.com>
2024-01-06 16:31:27 +0900

    백준 수 정렬하기2 리뷰

--- review.py
+++ review.py
@@ -1,20 +1,32 @@
-'''
-정렬 전후 index값의 차이의 최대값은 swap의 횟수와 같다
-'''
-
 import sys
 input = sys.stdin.readline
-
 N = int(input())
 A = []
-for i in range(N):
-    A.append((int(input()), i))
-A.sort()
+for _ in range(N):
+    A.append(int(input()))
 
-answer = 0
-for idx, a in enumerate(A):
-    num, i = a
-    if (i-idx) > answer:
-        answer = i-idx
+def solve(arr):
+    if len(arr) == 1:
+        return arr
+    N = len(arr) // 2
+    left, right = solve(arr[:N]), solve(arr[N:])
+    sorted_arr = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            sorted_arr.append(left[i])
+            i += 1
+        else:
+            sorted_arr.append(right[j])
+            j += 1
+    if i < len(left):
+        for p in range(i, len(left)):
+            sorted_arr.append(left[p])
+    elif j < len(right):
+        for q in range(j, len(right)):
+            sorted_arr.append(right[q])
+    return sorted_arr
 
-print(answer+1)
+sorted_A = solve(A)
+for a in sorted_A:
+    print(a)
