556b704ac2588eb364f50d90551b214359afd72a
swwho96 <swwho96@gmail.com>
2024-01-07 17:37:56 +0900

    백준 연결된 요소의 개수 리뷰

--- review.py
+++ review.py
@@ -1,32 +1,29 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-N = int(input())
-A = []
-for _ in range(N):
-    A.append(int(input()))
 
-def solve(arr):
-    if len(arr) == 1:
-        return arr
-    N = len(arr) // 2
-    left, right = solve(arr[:N]), solve(arr[N:])
-    sorted_arr = []
-    i, j = 0, 0
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            sorted_arr.append(left[i])
-            i += 1
-        else:
-            sorted_arr.append(right[j])
-            j += 1
-    if i < len(left):
-        for p in range(i, len(left)):
-            sorted_arr.append(left[p])
-    elif j < len(right):
-        for q in range(j, len(right)):
-            sorted_arr.append(right[q])
-    return sorted_arr
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-sorted_A = solve(A)
-for a in sorted_A:
-    print(a)
+visited = [False] * (N+1)
+def bfs(start):
+    q = deque([start])
+    visited[start] = True
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            if visited[i] is False:
+                q.append(i)
+                visited[i] = True
+
+cnt = 0
+for i in range(1, N+1):
+    if visited[i] is False:
+        bfs(i)
+        cnt += 1
+
+print(cnt)
