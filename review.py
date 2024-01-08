5e678ccd9fb2076d67f4b255eb0a96e20c767fd1
swwho96 <swwho96@gmail.com>
2024-01-08 12:46:07 +0900

    백준 기타레슨 리뷰

--- review.py
+++ review.py
@@ -1,23 +1,30 @@
+'''
+블루레이의 크기를 줄여가면서, 해당 크기의 블루레이로 만든 동영상의 개수가 M개를 넘지 않는 경우를 구한다
+'''
+
 import sys
-from collections import deque
 input = sys.stdin.readline
+N, M = map(int, input().split())
+videos = list(map(int, input().split()))
+start, end = max(videos), sum(videos)
 
-N = int(input())
-answer = []
 
-# 소수 구하기
-def is_Prime(n):
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
-    return True
+while start <= end:
+    size = (start+end) // 2
+    # 블루레이 만들기
+    blueray_cnt = 0
+    tmp_sum = 0
+    for i in range(N):
+        if tmp_sum+videos[i] <= size:
+            tmp_sum += videos[i]
+        elif tmp_sum+videos[i] > size:
+            tmp_sum = videos[i]
+            blueray_cnt += 1
+    if tmp_sum > 0: blueray_cnt += 1
+    # 블루레이 확인
+    if blueray_cnt > M:
+        start = size + 1
+    elif blueray_cnt <= M:
+        end = size - 1
 
-for n in range(10**(N-1), 10**(N)):
-    str_n = str(n)
-    flag = True
-    for idx in range(N):
-        if is_Prime(int(str_n[:idx+1])) is False:
-            flag = False
-            break
-    if flag is True:
-        print(n)
+print(start)
