b575364536f7010cb0e719d02ca91a646b328d19
swwho96 <swwho96@gmail.com>
2024-01-08 20:43:36 +0900

    백준 K번째 수 리뷰

--- review.py
+++ review.py
@@ -1,30 +1,19 @@
-'''
-블루레이의 크기를 줄여가면서, 해당 크기의 블루레이로 만든 동영상의 개수가 M개를 넘지 않는 경우를 구한다
-'''
-
 import sys
 input = sys.stdin.readline
-N, M = map(int, input().split())
-videos = list(map(int, input().split()))
-start, end = max(videos), sum(videos)
+N = int(input())
+K = int(input())
 
-
+start, end = 1, K
 while start <= end:
-    size = (start+end) // 2
-    # 블루레이 만들기
-    blueray_cnt = 0
-    tmp_sum = 0
-    for i in range(N):
-        if tmp_sum+videos[i] <= size:
-            tmp_sum += videos[i]
-        elif tmp_sum+videos[i] > size:
-            tmp_sum = videos[i]
-            blueray_cnt += 1
-    if tmp_sum > 0: blueray_cnt += 1
-    # 블루레이 확인
-    if blueray_cnt > M:
-        start = size + 1
-    elif blueray_cnt <= M:
-        end = size - 1
+    mid = (start+end) // 2
+    tmp =  0
+    for i in range(1, N+1):
+        tmp += min(mid//i, N)
 
-print(start)
+    if tmp >= K:
+        answer = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(answer)
