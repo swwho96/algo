c4e29abd7b779ab59c098ebda91fe3341c81b9e9
swwho96 <swwho96@gmail.com>
2024-01-15 16:24:47 +0900

    백준 효율적인 해킹 리뷰

--- baekjoon/container7/baekjoon_1325.py
+++ baekjoon/container7/baekjoon_1325.py
@@ -1,33 +1,35 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
+
 n, m = map(int, input().split())
 computer = [[] for _ in range(n+1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    computer[a].append(b)
-
-result = [0] * (n+1)
+    computer[b].append(a)
 
 # bfs
 def bfs(v):
     q = deque([v])
     visited[v] = True
+    cnt = 0
     while q:
         now = q.popleft()
         for i in computer[now]:
             if visited[i] is False:
                 visited[i] = True
-                result[i] += 1
+                cnt += 1
                 q.append(i)
+    return cnt
 
 # 모든 컴퓨터 확인
+result = []
 for c in range(1, n+1):
     visited = [False] * (n+1)
-    bfs(c)
+    result.append(bfs(c))
 
 # 가장 많은 값
 maxResult = max(result)
-for idx, i in enumerate(result):
+for idx, i in enumerate(result, start=1):
     if i == maxResult:
         print(idx, end=' ')
