d397f6c75fdc7b91889efa181ece5634b624bdb4
swwho96 <swwho96@gmail.com>
2024-01-19 13:01:44 +0900

    백준 최소 비용 구하기 리뷰

--- review.py
+++ review.py
@@ -2,22 +2,22 @@ import heapq
 import sys
 input = sys.stdin.readline
 
-V, E = map(int, input().split())
-K = int(input())
+V = int(input())
+E = int(input())
 graph = [[] for _ in range(V+1)]
 for _ in range(E):
     u, v, w = map(int, input().split())
     graph[u].append((v, w))
+start, end = map(int, input().split())
 
-q = [(0, K)]
+q = [(0, start)]
 distance = [int(10e9)] * (V+1)
-distance[K] = 0
+distance[start] = 0
 while q:
     cost, now = heapq.heappop(q)
     for i in graph[now]:
         if distance[i[0]] > distance[now] + i[1]:
             distance[i[0]] = distance[now] + i[1]
-            heapq.heappush(q, (distance[i[0]], i[0]))
+            heapq.heappush(q, (distance[i[0]], i[0]))   
 
-for i in range(1, V+1):
-    print(distance[i] if distance[i] < int(10e9) else "INF")
+print(distance[end])
