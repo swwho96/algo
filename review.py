a18a54b96b260e531e5c728a3c2e3129776e88eb
swwho96 <swwho96@gmail.com>
2024-01-19 12:51:52 +0900

    백준 최단 경로 리뷰

--- review.py
+++ review.py
@@ -1,32 +1,23 @@
+import heapq
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-N = int(input())
-graph = [[] for _ in range(N+1)]
-indegree = [0] * (N+1)
-build_time = [0] * (N+1)
-result = [0] * (N+1)
-for i in range(1, N+1):
-    infos = list(map(int, input().split()))[:-1]
-    result[i] = infos[0]
-    for bilding in infos[1:]:
-        graph[bilding].append(i)
-        indegree[i] += 1
+V, E = map(int, input().split())
+K = int(input())
+graph = [[] for _ in range(V+1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
 
-q = deque([])
-for i in range(1, N+1):
-    if indegree[i] == 0:
-        q.append(i)
-
+q = [(0, K)]
+distance = [int(10e9)] * (V+1)
+distance[K] = 0
 while q:
-    now = q.popleft()
+    cost, now = heapq.heappop(q)
     for i in graph[now]:
-        build_time[i] = max(build_time[i], result[now])
-        indegree[i] -= 1
-        if indegree[i] == 0:
-            result[i] += build_time[i]
-            q.append(i)
+        if distance[i[0]] > distance[now] + i[1]:
+            distance[i[0]] = distance[now] + i[1]
+            heapq.heappush(q, (distance[i[0]], i[0]))
 
-for i in range(1, N+1):
-    print(result[i])
+for i in range(1, V+1):
+    print(distance[i] if distance[i] < int(10e9) else "INF")
