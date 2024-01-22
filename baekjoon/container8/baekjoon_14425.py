10b6b656276dbe534f6973cfc501525f8e1bfa0c
swwho96 <swwho96@gmail.com>
2024-01-22 16:50:00 +0900

    문자열 집합

---
+++ baekjoon/container8/baekjoon_14425.py
@@ -0,0 +1,60 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+class Node:
+    def __init__(self, key, data=None):
+        self.key = key
+        self.data = data
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char not in current_node.children:
+                current_node.children[char] = Node(char)
+            current_node = current_node.children[char]
+        current_node.data = string
+
+    def search(self, string):
+        current_node = self.head
+
+        for char in string:
+            if char in current_node.children:
+                current_node = current_node.children[char]
+            else:
+                return False
+
+        if current_node.data:
+            return True
+        else:
+            return False
+
+    def starts_with(self, prefix):
+        current_node = self.head
+        words = []
+
+        for p in prefix:
+            if p in current_node.children:
+                current_node = current_node.children[p]
+            else:
+                return None
+            
+trie = Trie()
+for _ in range(N):
+    string = input().rstrip()
+    trie.insert(string)
+
+result = 0
+for _ in range(M):
+    string = input().rstrip()
+    if trie.search(string) is True:
+        result += 1
+
+print(result)
