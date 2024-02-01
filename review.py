e25257f2d799100d1382ffc73c974fc9afa1570c
swwho96 <swwho96@gmail.com>
2024-02-01 18:16:42 +0900

    백준 부녀회장이 될테야 리뷰

--- review.py
+++ review.py
@@ -1,51 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-N, M, K = map(int, input().split())
-tmp = N
-treesize = 0
-while tmp != 0:
-    tmp //= 2
-    treesize += 1
-
-treesize = pow(2, treesize+1)
-leftstart = treesize // 2 - 1
-tree = [0] * (treesize+1)
-
-for idx in range(leftstart+1, leftstart+N+1):
-    tree[idx] = int(input().rstrip())
-
-def initTree(i):
-    while i != 1:
-        tree[i//2] += tree[i]
-        i -= 1
-
-initTree(treesize-1)
-
-def change(index, value):
-    diff = value-tree[index]
-    while index > 0:
-        tree[index] += diff
-        index = index // 2
-
-def getsum(s, e):
-    answer = 0
-    while s <= e:
-        if s % 2 == 1:
-            answer += tree[s]
-            s += 1
-        if e % 2 == 0:
-            answer += tree[e]
-            e -= 1
-        s //= 2
-        e //= 2
-    return answer
-
-for _ in range(M+K):
-    q, s, e = map(int, input().split())
-    if q == 1:
-        change(leftstart+s, e)
-    elif q == 2:
-        s += leftstart
-        e += leftstart
-        print(getsum(s,e))
+T = int(input())
+for _ in range(T):
+    K = int(input()) # 층
+    N = int(input()) # 호
+    apt = [[1] * (N+1) for _ in range(K+1)]
+    # 0층 i호는 i명 거주
+    for i in range(1, N+1):
+        apt[0][i] = i
+    # a층 b호는 a-1층 1호부터 b호까지의 총 인원
+    for a in range(1, K+1):
+        for b in range(2, N+1):
+            apt[a][b] = apt[a][b-1] + apt[a-1][b]
+    # K층 N호 인원
+    print(apt[K][N])
