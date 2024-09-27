c40531f91e1dd3fe5fbbfb99715b5f48aa43fac9
swwho96 <swwho96@gmail.com>
2024-09-27 18:33:52 +0900

    11725 재풀이

--- review.py
+++ review.py
@@ -1,16 +1,23 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-dp_0 = [0,0,1]
-dp_1 = [0,1,0]
+N = int(input().rstrip())
+graph = [[] for _ in range(N+1)]
+tree = {i: {'child':[], 'parent':None} for i in range(1,N+1)}
+for _ in range(N-1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-N = int(input())
-if N <= 2:
-    print(dp_0[N]+dp_1[N])
-else:
-    for _ in range(N-2):
-        tmp_0 = dp_0[-1]+dp_1[-1]
-        tmp_1 = dp_0[-1]
-        dp_0.append(tmp_0)
-        dp_1.append(tmp_1)
-    print(dp_0[N]+dp_1[N])
+def makeTree(cur:int, parent:int):
+    global tree
+    for node in graph[cur]:
+        if node != parent:
+            tree[cur]['child'].append(node)
+            tree[node]['parent'] = cur
+            makeTree(node, cur)
+
+makeTree(1, -1)
+for i in range(2, N+1):
+    print(tree[i]['parent'])
