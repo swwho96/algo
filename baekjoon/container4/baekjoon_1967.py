4c02d983283174e1a020d2337d5cbbfd68a5d522
swwho96 <swwho96@gmail.com>
2024-09-28 12:32:56 +0900

    1967번 재풀이

--- baekjoon/container4/baekjoon_1967.py
+++ baekjoon/container4/baekjoon_1967.py
@@ -26,3 +26,32 @@ visited[start] = 0
 dfs(start, 0)
 
 print(max(visited))
+
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+
+# N = int(input().rstrip())
+# graph = [[] for _ in range(N+1)]
+# for _ in range(N-1):
+#     a, b, cost = map(int, input().split())
+#     graph[a].append((b, cost))
+#     graph[b].append((a, cost))
+
+# def tree_bfs(start:int)->int:
+#     dists = [0] * (N+1)
+#     dists[start] = 0
+#     q = deque([start])
+#     while q:
+#         now = q.popleft()
+#         for node, cost in graph[now]:
+#             if dists[node] == 0 and node != start:
+#                 dists[node] = dists[now] + cost
+#                 q.append(node)
+#     return dists
+
+# dists_from_root = tree_bfs(1)
+# start = dists_from_root.index(max(dists_from_root))
+# result_dists = tree_bfs(start)
+# print(dists_from_root, start, result_dists, result_dists.index(max(result_dists)))
+# print(max(result_dists))
