23edd5275458dcc8f17b227bbb28653656770317
swwho96 <swwho96@gmail.com>
2024-10-12 12:02:54 +0900

    방 번호

--- baekjoon/container11/baekjoon_1475.py
+++ baekjoon/container11/baekjoon_1475.py
@@ -1,7 +1,5 @@
-for i in range(1000000):
-    N = str(i)
-    counter = {str(i):0 for i in range(10)}
-    for n in N:
-        counter[n] += 1
-    # print(max(max(counter[c] for c in '01234578'), (counter['9']+counter['6']+1)//2))
-    tmp = max(max(counter[c] for c in '01234578'), (counter['9']+counter['6']+1)//2)
+N = input()
+counter = {str(i):0 for i in range(10)}
+for n in N:
+    counter[n] += 1
+print(max(max(counter[c] for c in '01234578'), (counter['9']+counter['6']+1)//2))
