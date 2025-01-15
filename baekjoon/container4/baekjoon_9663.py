ee31e0b73878be53810b37a17200ff271e47e84f
swwho96 <swwho96@gmail.com>
2025-01-16 00:04:14 +0900

    N-Queen 복습

--- baekjoon/container4/baekjoon_9663.py
+++ baekjoon/container4/baekjoon_9663.py
@@ -1,27 +1,24 @@
 n = int(input())
-result = 0
-board = [0] * n
-visited = [False] * n
 
-def queen(row):
-    global result
+def put_queen(row:int):
+    global left_right, yx, y_x, cnt, n
     if row == n:
-        result += 1
+        cnt += 1
         return
     for i in range(n):
-        if visited[i]:
-            continue
-        board[row] = i
-        if check(row):
-            visited[i] = True
-            queen(row+1)
-            visited[i] = False
+        if left_right[i] is False and yx[row+i] is False and y_x[(n-1) + row-i] is False:
+            left_right[i] = True
+            yx[row+i] = True
+            y_x[(n-1)+row-i] = True
+            put_queen(row+1) 
+            left_right[i] = False
+            yx[row+i] = False
+            y_x[(n-1)+row-i] = False
 
-def check(row):
-    for i in range(row):
-        if (board[row] == board[i]) or (abs(row-i) == abs(board[row]-board[i])):
-            return False
-    return True
 
-queen(0)
-print(result)
+left_right = [False] * n
+yx = [False] * (2*(n-1)+1)
+y_x = [False] * (2*(n-1)+1)
+cnt = 0
+put_queen(0)
+print(cnt)
